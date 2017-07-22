246462142c793d94df48913dffe5528459dc9114
gilbertjacke <gilbertjacke@gmail.com>
2017-07-22 11:22:46 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 3001;
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+
+app.use('/'
+
+
+app.listen(port);
+console.log("All wathced over port " + port + " of loving grace. ");
