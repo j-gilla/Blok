a954ef697c5f254a81c7d99317e88672532b0f9a
gilbertjacke <gilbertjacke@gmail.com>
2017-07-30 16:44:16 +0100

    react-router4 upgrade

--- client/index.js
+++ client/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 import App from './src/App.jsx';
 import Login from './src/Login.jsx';
 
 
-const Root = () =>{
-    return(
-        <Router history={hashHistory}>
-            <Route path ="/" component={App} />
-            <IndexRoute component ={Login} />
-    </Router>
-    )
-};
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+render(<App />,document.getElementById('root'));
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-class App extends React.Component{
+export default class App extends React.Component{
   render() {
     return(
-      <h1>Aiya</h1>
+      <h1> App - HIYA</h1>
     )
   }
 };
-export default App;
